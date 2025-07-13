e7146a5f167509690c922c28549e3b2f06eefb43
martinodb <martinobal@gmail.com>
2025-07-13 17:13:03 +0200

    cleanup

--- nlpsolver.py
+++ nlpsolver.py
@@ -102,8 +102,7 @@ controlling the prover:
 def server_answer_question(text, newoptions=None):
   """Call the nlpserver to solve a question with options."""
   debug_print("server_answer_question text", text)
-  # if newoptions: # This is now done in the answer_question function
-  #   set_global_options(newoptions)
+
     
   conn = http.client.HTTPConnection(nlpglobals.server_name, nlpglobals.server_port, timeout=nlpglobals.server_timeout)
   
