d6cfbe37ab6f9497c09d587a6efe59e5d300ab04
martinodb <martinobal@gmail.com>
2025-07-16 20:28:50 +0200

    commented out what's not needed

--- martinodb_nlpclient.py
+++ martinodb_nlpclient.py
@@ -38,21 +38,23 @@ import json
 # ==== import other source files ====
 
 # configuration and other globals are in nlpglobals.py
-from nlpglobals import *
+import nlpglobals
+from nlpglobals import options
+
 
 # the core ud-to-logic converter parts are in nlptologic.py
-from nlptologic import *
+# from nlptologic import *
 
 # prover calling and prover result conversion parts are in nlpprover.py
-from nlpprover import *
+# from nlpprover import *
 
 # optional llm simplifications are in nlpllm
-from nlpllm import *
+# from nlpllm import *
 
 # small utilities are in nlputils.py
-from nlputils import *
+from nlputils import debug_print, show_error
 
-from nlpcache import *
+# from nlpcache import *
 
 # ====== main is used for calling on command line ======
 
@@ -65,8 +67,8 @@ from nlpcache import *
 # explain the result:   ./nlpsolver "Elephants are animals. John is an elephant. John is an animal?" -explain
 # processing details:   ./nlpsolver "Elephants are animals. John is an elephant. John is an animal?" -debug
 
-import datetime 
-from datetime import timedelta 
+# import datetime 
+# from datetime import timedelta 
 
 helptext="""call nlpsolver with a natural language text like
 "Elephants are big. John is an elephant. Who is big?"
@@ -175,92 +177,92 @@ def main():
 # ====== answer_question carries out the whole process ======
 
 
-def answer_question(text,newoptions=None):
-  debug_print("answer_question text",text)    
-  if newoptions: set_global_options(newoptions) 
-  # --- read textual replacement rules ---
-  global replacement_complex_rules
-  rules=read_replacement_data()
-  nlpglobals.replacement_text_rules=rules[0]
-  nlpglobals.replacement_complex_rules=rules[1]
-  #print(nlpglobals.replacement_text_rules)
-  #return
-  # - - - make optional llm simplifications - - - -
-  llm_mapping=None
-  if options["llm_flag"]:
-    simpres=llm_simplify(text)
-    text=simpres[0]
-    llm_mapping=simpres[1]
-  # - - - do basic text replacements - - - -
-  text=prepare_text(text)     
-  debug_print("answer_question prepared text",text)  
-  # - - - call ud parser via server - - - 
-  data = server_parse(text)
-  doc=data["doc"]  
-  entities=data["entities"] 
-  debug_print("doc tree:\n")
-  debug_print_sentence_trees(doc)     
-  debug_print("entities",entities)  
-  # - - - convert to logic and solve - - - 
-  logic_objects=parse_ud(doc,entities)    
-  #debug_print("logic_objects",logic_objects)
+# def answer_question(text,newoptions=None):
+#   debug_print("answer_question text",text)    
+#   if newoptions: set_global_options(newoptions) 
+#   # --- read textual replacement rules ---
+#   global replacement_complex_rules
+#   rules=read_replacement_data()
+#   nlpglobals.replacement_text_rules=rules[0]
+#   nlpglobals.replacement_complex_rules=rules[1]
+#   #print(nlpglobals.replacement_text_rules)
+#   #return
+#   # - - - make optional llm simplifications - - - -
+#   llm_mapping=None
+#   if options["llm_flag"]:
+#     simpres=llm_simplify(text)
+#     text=simpres[0]
+#     llm_mapping=simpres[1]
+#   # - - - do basic text replacements - - - -
+#   text=prepare_text(text)     
+#   debug_print("answer_question prepared text",text)  
+#   # - - - call ud parser via server - - - 
+#   data = server_parse(text)
+#   doc=data["doc"]  
+#   entities=data["entities"] 
+#   debug_print("doc tree:\n")
+#   debug_print_sentence_trees(doc)     
+#   debug_print("entities",entities)  
+#   # - - - convert to logic and solve - - - 
+#   logic_objects=parse_ud(doc,entities)    
+#   #debug_print("logic_objects",logic_objects)
 
-  if "logic" in logic_objects: logic=logic_objects["logic"]
-  else: logic=None
-  if "objects" in logic_objects: objects=logic_objects["objects"]
-  else: objects=None
-  if "question_sentence" in logic_objects: question_sentence=logic_objects["question_sentence"]
-  else: question_sentence=None
-  if "question_type" in logic_objects: question_type=logic_objects["question_type"]
-  else: question_type=None
-  if "logic_sentence_map" in logic_objects: logic_sentence_map=logic_objects["logic_sentence_map"]
-  else: logic_sentence_map=None
-  #debug_print("logic")
-  #debug_print_logic_list(logic)
-  #debug_print("objects",objects)
-  #debug_print("question_sentence",question_sentence)
-  #debug_print("logic_sentence_map",logic_sentence_map)
-  #debug_print("")
-  origlogic=logic
-  if options["debug_print_flag"] or options["show_logic_flag"]:     
-    print()
-    show_objects(logic_objects) 
-    print()
-  if options["show_logic_flag"]: 
-    print()    
-    show_sentence_clauses(logic_objects)     
-  if options["show_logic_flag"]:    
-    print()
-    print("=== result: ===\n")  
+#   if "logic" in logic_objects: logic=logic_objects["logic"]
+#   else: logic=None
+#   if "objects" in logic_objects: objects=logic_objects["objects"]
+#   else: objects=None
+#   if "question_sentence" in logic_objects: question_sentence=logic_objects["question_sentence"]
+#   else: question_sentence=None
+#   if "question_type" in logic_objects: question_type=logic_objects["question_type"]
+#   else: question_type=None
+#   if "logic_sentence_map" in logic_objects: logic_sentence_map=logic_objects["logic_sentence_map"]
+#   else: logic_sentence_map=None
+#   #debug_print("logic")
+#   #debug_print_logic_list(logic)
+#   #debug_print("objects",objects)
+#   #debug_print("question_sentence",question_sentence)
+#   #debug_print("logic_sentence_map",logic_sentence_map)
+#   #debug_print("")
+#   origlogic=logic
+#   if options["debug_print_flag"] or options["show_logic_flag"]:     
+#     print()
+#     show_objects(logic_objects) 
+#     print()
+#   if options["show_logic_flag"]: 
+#     print()    
+#     show_sentence_clauses(logic_objects)     
+#   if options["show_logic_flag"]:    
+#     print()
+#     print("=== result: ===\n")  
   
-  if (not options["prover_nosolve_flag"] and origlogic and 
-      (not list_contains_key(origlogic,"@question")) and 
-      ("result" not in logic_objects)):
-    return "No question given."   
-    #print("No question given.")
-    #sys.exit(0)
-  if "result" not in logic_objects:
-    rawresult=call_prover(logic)
-  elif logic_objects["result"]: 
-    if options["prover_explain_flag"]:
-      print("Answer found without proof search:")   
-    return logic_objects["result"]  
-  else:
-    if options["prover_explain_flag"]:
-      print("Answer found without proof search:")
-    return "Unknown."  
+#   if (not options["prover_nosolve_flag"] and origlogic and 
+#       (not list_contains_key(origlogic,"@question")) and 
+#       ("result" not in logic_objects)):
+#     return "No question given."   
+#     #print("No question given.")
+#     #sys.exit(0)
+#   if "result" not in logic_objects:
+#     rawresult=call_prover(logic)
+#   elif logic_objects["result"]: 
+#     if options["prover_explain_flag"]:
+#       print("Answer found without proof search:")   
+#     return logic_objects["result"]  
+#   else:
+#     if options["prover_explain_flag"]:
+#       print("Answer found without proof search:")
+#     return "Unknown."  
 
-  # - - - show the result - - - 
+#   # - - - show the result - - - 
 
-  if options["prover_nosolve_flag"]:
-    return rawresult
-  elif options["prover_rawresult_flag"]:
-    return rawresult  
-  elif rawresult and rawresult.startswith("Error"):
-    return rawresult  
+#   if options["prover_nosolve_flag"]:
+#     return rawresult
+#   elif options["prover_rawresult_flag"]:
+#     return rawresult  
+#   elif rawresult and rawresult.startswith("Error"):
+#     return rawresult  
  
-  niceresult=make_nlp_result(doc,rawresult,logic_objects,question_type,llm_mapping)
-  return niceresult
+#   niceresult=make_nlp_result(doc,rawresult,logic_objects,question_type,llm_mapping)
+#   return niceresult
 
 # detect input strings, filenames and optional keys
 
@@ -394,59 +396,59 @@ def parse_cmd_line(helptext):
     elif textpart: text=textpart  
   return (text,options)
 
-def prepare_text(text):
-  #print("orig text",text)
-  if not text: return ""
-  text=text.replace("%"," percent")
-  # --- put whitespace in front of periods for variable. name ---
-  newspln=[]
-  spln=text.split(" ")
-  for el in spln:
-    if el and el[-1]=="." and variable_shaped_lemma(el[:-1]):
-      newspln.append(el[:-1])
-      newspln.append(".")
-    else:
-      newspln.append(el)  
-  text=" ".join(newspln)
+# def prepare_text(text):
+#   #print("orig text",text)
+#   if not text: return ""
+#   text=text.replace("%"," percent")
+#   # --- put whitespace in front of periods for variable. name ---
+#   newspln=[]
+#   spln=text.split(" ")
+#   for el in spln:
+#     if el and el[-1]=="." and variable_shaped_lemma(el[:-1]):
+#       newspln.append(el[:-1])
+#       newspln.append(".")
+#     else:
+#       newspln.append(el)  
+#   text=" ".join(newspln)
 
-  # --- put missing commas in front of then in if-sentences 
-  spln=text.split("\n")
-  newspl=[]
-  for line in spln:    
-    splp=line.split(".")
-    newsplp=[]
-    for sentence in splp:
-      pos=None
-      if sentence.startswith("If "): pos=0
-      if not pos: 
-        pos=sentence.find(" If ")
-        if pos<0: pos=sentence.find(" if ")
-        if pos<0: pos=sentence.find(" IF ")
-      commapos=None  
-      if pos!=None:
-        tpos=sentence.find(" then ",pos)
-        if tpos:
-          i=tpos
-          while i>0:
-            if sentence[i]==" ": 
-              i=i-1              
-            elif sentence[i].isalnum(): 
-              commapos=i
-              break
-            else:
-              break
+#   # --- put missing commas in front of then in if-sentences 
+#   spln=text.split("\n")
+#   newspl=[]
+#   for line in spln:    
+#     splp=line.split(".")
+#     newsplp=[]
+#     for sentence in splp:
+#       pos=None
+#       if sentence.startswith("If "): pos=0
+#       if not pos: 
+#         pos=sentence.find(" If ")
+#         if pos<0: pos=sentence.find(" if ")
+#         if pos<0: pos=sentence.find(" IF ")
+#       commapos=None  
+#       if pos!=None:
+#         tpos=sentence.find(" then ",pos)
+#         if tpos:
+#           i=tpos
+#           while i>0:
+#             if sentence[i]==" ": 
+#               i=i-1              
+#             elif sentence[i].isalnum(): 
+#               commapos=i
+#               break
+#             else:
+#               break
            
-      if commapos!=None:
-        newsentence=sentence[:commapos+1]+","+sentence[commapos+1:]
-      else:        
-        newsentence=sentence
-      newsplp.append(newsentence)
+#       if commapos!=None:
+#         newsentence=sentence[:commapos+1]+","+sentence[commapos+1:]
+#       else:        
+#         newsentence=sentence
+#       newsplp.append(newsentence)
 
-    sentres=".".join(newsplp)
-    newspl.append(sentres)
+#     sentres=".".join(newsplp)
+#     newspl.append(sentres)
 
-  text="\n".join(newspl)
-  return text
+#   text="\n".join(newspl)
+#   return text
 
 
 # =========== main caller ==========
